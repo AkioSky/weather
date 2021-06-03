410f7827641fd2ab6b8d1d673a9255360a1e7762
Aquari <daniels1983@mail.com>
2021-06-03 16:50:12 +0800

    fix url request

--- core/views.py
+++ core/views.py
@@ -13,7 +13,11 @@ def index(request):
         lat = 51.5
         lon = -0.25
     url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={}&lon={}'.format(lat, lon)
-    weather_data = requests.get(url).json()
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64;"}
+
+    weather_data = requests.get('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=-0.25',
+                                headers=headers)
+    print(weather_data.text)
     return render(request, 'core/index.html', context={
         'data': weather_data
     })
